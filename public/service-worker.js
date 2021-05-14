57e2004146477dbffc10b93bbb1b811a2a37a218
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-05-14 08:27:14 -0500

    created service worker, linked to HTML. Service worker includes CACHE variables and FILES TO CACHE

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/style.css",
+    "/public/icons/icon-192x192.png",
+    "/public/icons/icon-512x512.png"
+];
