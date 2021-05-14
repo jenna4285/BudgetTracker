a7d8af62072593a2ff057686e1ee7f85fae14e52
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-05-14 08:30:10 -0500

    created service worker install function

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,13 @@ const FILES_TO_CACHE = [
     "/public/icons/icon-192x192.png",
     "/public/icons/icon-512x512.png"
 ];
+
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    );
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+    self.skipWaiting();
+  });
